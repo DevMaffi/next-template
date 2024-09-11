5545d927e32d4f8ff8aca8e358cbd007987b7ff6
DevMaffi <devmaffi550@gmail.com>
2024-09-11 15:56:18 +0300

    chore(eslint): add @eslint/js rules (96/197)

--- eslint/eslint-config-js.mjs
+++ eslint/eslint-config-js.mjs
@@ -10,7 +10,6 @@ const config = [
             "arrow-body-style": "error", // 💡 enforces no braces where they can be omitted
             "block-scoped-var": "error",
             "camelcase": "error",
-
             "array-callback-return": [
                 "error",
                 {
@@ -18,7 +17,6 @@ const config = [
                     allowVoid: true,
                 },
             ],
-
             "capitalized-comments": [
                 "warn",
                 "always",
@@ -35,12 +33,42 @@ const config = [
             "eqeqeq": "error",
             "func-name-matching": "error",
             "func-names": "off", // ❌ 💡 requires function expressions to have a name
-
             "func-style": [
                 "error",
                 "declaration",
                 { allowArrowFunctions: true }, // ⚠️ prefer-arrow-callback
             ], // 💡 requires the use of function declarations instead of function expressions assigned to variables
+
+            "guard-for-in": "off", // ❌
+            "id-denylist": ["error", "z"], // TODO: complete list
+            "init-declarations": "error",
+            "logical-assignment-operators": ["error", "never"],
+            "id-length": [
+                "error",
+                {
+                    min: 3,
+                    properties: "never",
+                    exceptions: ["_"],
+                },
+            ],
+
+            "no-alert": "error",
+            "no-await-in-loop": "error",
+            "no-bitwise": "error",
+            "no-caller": "error",
+            "new-cap": [
+                "error",
+                { capIsNew: false },
+            ],
+
+            "no-cond-assign": ["error", "always"], // ☑️
+            "no-console": "error",
+            "no-continue": "error",
+            "no-div-regex": "error", // 💡 characters /= at the beginning of a regular expression literal can be confused with a division assignment operator
+            "no-duplicate-imports": [
+                "error",
+                { includeExports: true },
+            ],
         },
     },
 ]
