be699056c50ea613f444e77f5a0d678d82d5f4d2
DevMaffi <devmaffi550@gmail.com>
2024-10-12 18:33:13 +0300

    refactor(eslint): update config to enable auto fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,19 @@
 import js from "@eslint/js"
 import tseslint from "typescript-eslint"
 import globals from "globals"
+import react from "eslint-plugin-react"
 
 import jsConfig from "./eslint/eslint-config-js.mjs"
-import react from "./eslint/eslint-config-react.mjs"
 import misc from "./eslint/eslint-config-misc.mjs"
 import stylistic from "./eslint/eslint-config-stylistic.mjs"
+import reactConfig from "./eslint/eslint-config-react.mjs"
 
 /**
  * @type {import("typescript-eslint").ConfigWithExtends[]}
  */
 const combineConfigs = [
     ...jsConfig,
-    ...react,
+    ...reactConfig,
     ...misc,
     ...stylistic,
 ]
@@ -26,7 +27,7 @@ export default tseslint.config(
             ecmaVersion: 2022,
             sourceType: "module",
             globals: globals.browser,
-            parserOptions: ["tsconfig.json"],
+            parserOptions: react.configs.parserOptions,
         },
     },
     ...combineConfigs.map(config => ({
